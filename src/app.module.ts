3a4e1975ad1c436d6a4ae6c33e3c1533bebb404a
Sanyam Jain <sanyamjain10168@gmail.com>
2023-11-04 10:37:12 +0530

    added serverless db

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,33 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './Product/product.module';
 import { CartModule } from './Cart/cart.module';
 
+
+// TypeOrmModule.forRoot({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "root",
+//   password: "root",
+//   database: "test_db",
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: true,
+// }),
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({isGlobal:true}),
     ProductModule,
     CartModule,
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "root",
-      password: "root",
-      database: "test_db",
+      host: process.env.PG_HOST,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl:true,
+      autoLoadEntities:true
     }),
   ],
   controllers: [AppController],
